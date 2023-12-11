26f15f1b09328453e6961b602c5c9a2f49dbffaa
Ege Kavcıoğlu <sonicster1458@hotmail.com>
2023-12-11 18:49:03 -0200

    Homework 6.12.2023

--- demo/src/main/java/com/example/demo/entities/Address.java
+++ demo/src/main/java/com/example/demo/entities/Address.java
@@ -1,12 +1,10 @@
 package com.example.demo.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import java.util.List;
 
 @Entity
 @Table(name="address")
